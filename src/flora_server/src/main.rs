44d442f91e25401bfb47ca485b51cc921c7a5df1
damillora@damillora.com <damillora@damillora.com>
2026-07-02 02:54:09 +0700

    feat: cleanup of Flora gRPC server sockets

--- src/flora_server/src/main.rs
+++ src/flora_server/src/main.rs
@@ -1,6 +1,6 @@
 use directories::BaseDirs;
 use flora_core::{errors::FloraError, manager::FloraManager};
-use tokio::net::UnixListener;
+use tokio::{net::UnixListener, signal::{self}};
 use tokio_stream::wrappers::UnixListenerStream;
 use tonic::transport::Server;
 
@@ -17,17 +17,28 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut uds_socket_path = base_dirs.runtime_dir().ok_or(FloraError::NoValidHome)?.to_path_buf();
     uds_socket_path.push("flora-server.sock");
 
-    let uds = UnixListener::bind(uds_socket_path)?;
+    log::info!("Establishing socket...");
+    let uds = UnixListener::bind(&uds_socket_path)?;
     let incoming = UnixListenerStream::new(uds);
 
     let manager = FloraManager::new()?;
 
     let greeter = FloraManagerServiceImpl::new(manager);
 
+    let signal = signal::ctrl_c();
+
+    log::info!("Flora gRPC Server is running!");
     Server::builder()
         .add_service(FloraManagerServiceServer::new(greeter))
-        .serve_with_incoming(incoming)
+        .serve_with_incoming_shutdown(incoming, async {
+            signal.await.unwrap()
+        })
         .await?;
+
+    log::info!("Cleaning up...");
+    if uds_socket_path.exists() {
+        tokio::fs::remove_file(&uds_socket_path).await?;
+    }
 
     Ok(())
 }
